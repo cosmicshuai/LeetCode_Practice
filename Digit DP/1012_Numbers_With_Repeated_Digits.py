4ac03b7f44679f7a5543d939466ffbf18993265e
cosmicshuai <shuai_uf@outlook.com>
2022-09-04 08:43:46 +0800

    add a new solution for 1012

--- Digit DP/1012_Numbers_With_Repeated_Digits.py
+++ Digit DP/1012_Numbers_With_Repeated_Digits.py
@@ -31,3 +31,42 @@ class Solution:
         
         digits = getDigits(n)
         return n - getSepcialNumCnt(len(digits) - 1, True, True, 0, digits)
+
+# Do not use n - non-repeatDigitsNumber
+class Solution2:
+    def numDupDigitsAtMostN(self, n: int) -> int:
+        dp = {}
+        def getDupNumCnt(idx, tight, leadingZero, mask, digits):
+            if idx == -1:
+                return 0
+            if (idx, tight, leadingZero, mask) in dp and not tight:
+                return dp[(idx, tight, leadingZero, mask)]
+
+            ans = 0
+            k = digits[idx] if tight else 9
+            for i in range(k + 1):
+                nextTight = True if tight and i == digits[idx] else False
+                nextZero = True if i == 0 and leadingZero else False
+                if leadingZero and i == 0:
+                    ans += getDupNumCnt(idx - 1, nextTight, nextZero, mask, digits)
+                    continue
+                
+                if mask & 1 << i == 0:
+                    ans += getDupNumCnt(idx - 1, nextTight, nextZero, mask ^ 1 << i, digits)
+                else:
+                    ans += n % (10**idx) + 1 if nextTight else 10 ** idx
+
+            dp[(idx, tight, leadingZero, mask)] = ans
+            return ans
+
+        def getDigits(num):
+            ans = []
+            while num:
+                ans.append(num % 10)
+                num //= 10
+            return ans
+                
+        digits = getDigits(n)
+        return getDupNumCnt(len(digits) - 1, True, True, 0, digits)
+S = Solution2()
+print(S.numDupDigitsAtMostN(1000000000))
