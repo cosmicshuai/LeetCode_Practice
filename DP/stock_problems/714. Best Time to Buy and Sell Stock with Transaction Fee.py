9907de1cc34c6922faea9f4907e678108bd1f601
cosmicshuai <shuai_uf@outlook.com>
2022-10-05 13:49:56 -0400

    solution for 714

---
+++ DP/stock_problems/714. Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        dp = [[float('-inf')] * 2 for _ in range(n + 1)]
+
+        #base cases
+        dp[0][0] = 0
+
+        for i in range(1, n + 1):
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1])
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i - 1] - fee)
+
+        return dp[n][0]
