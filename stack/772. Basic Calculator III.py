7783ef2a3874b3a9a6c6b80e931f7cbfd2742cc4
cosmicshuai <shuai_uf@outlook.com>
2022-12-07 10:18:05 -0500

    Solution for 772

---
+++ stack/772. Basic Calculator III.py
@@ -0,0 +1,59 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        def isNumber(c):
+            return c in "0123456789"
+        elements = []
+        ops = "+-*/"
+        t = ""
+        for c in s:
+            if t.isnumeric() and isNumber(c):
+                t += c
+            elif t == " ":
+                continue
+            else:
+                if t != "":
+                    elements.append(t)
+                t = c
+        elements.append(t)
+        def operate(stack, op, num):
+            if op == "+":
+                stack.append(num)
+            elif op == "-":
+                stack.append(-num)
+            elif op == "*":
+                stack.append(stack.pop() * num)
+            else:
+                stack.append(int(stack.pop() / num))
+            
+        print(elements)
+        def process(eles):
+            n = len(eles)
+            i = 0
+            stack = []
+            op = "+"
+            cur = 0
+            while i < n:
+                t = eles[i]
+                if t.isnumeric():
+                    cur = int(t)
+                elif t in ops:
+                    operate(stack, op, cur)
+                    op = t
+                elif t == "(":
+                    cur, l = process(eles[i+1::])
+                    i += (l + 1)
+                elif t == ")":
+                    operate(stack, op, cur)
+                    return sum(stack), i
+                i += 1
+            operate(stack, op, cur)
+            print(stack)
+            return sum(stack)
+        
+        return process(elements)
+                    
+
+
+s = "1+(32-23/12)+8"
+S = Solution()
+print(S.calculate(s))
